6117ac17a990b7d1e5134bac2af0df94d3791f49
sang-w0o <robbyra@gmail.com>
2022-06-06 14:28:53 +0900

    feat: Add scripts for configuring dynamodb

---
+++ scripts/create-dynamodb-table.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+
+aws dynamodb create-table \
+  --table-name users \
+  --attribute-definitions \
+  AttributeName=user_id,AttributeType=S \
+  AttributeName=nickname,AttributeType=S \
+  AttributeName=created_at,AttributeType=S \
+  --key-schema \
+  AttributeName=user_id,KeyType=HASH \
+  --provisioned-throughput ReadCapacityUnits=10,WriteCapacityUnits=10 \
+  --global-secondary-indexes file://scripts/gsi.json \
+  --endpoint-url http://localhost:54000 \
+  --region ap-northeast-2 || true | cat
